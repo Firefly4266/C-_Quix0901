013ff2b36e4f519b2e118f795f807ad66f718695
Firefly4266 <gaines.johnp@gmail.com>
2017-09-06 12:07:50 -0700

    Started problem 3

--- Quiz_0901/Program.cs
+++ Quiz_0901/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 3)	Write a program that will ask for the user to supply the size of an array.        Then ask the user to enter enough random numbers to fill the array.  The        output of the program should be each number printed out on a single line        sorted in ascending order. */
+
 namespace Quiz_0901
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Please specify an array size (interger value): ");
+            string input = Console.ReadLine();
+            int size = int.Parse(input);
+
+            Console.Write("Enter intergers to fill the array to the number specified: ");
+            input = Console.ReadLine();
+            int value = int.Parse(input);
         }
     }
 }

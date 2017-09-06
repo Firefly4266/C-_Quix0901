cd02742adde7c9d1586d3d0d05cbd077f24f2bbf
Firefly4266 <gaines.johnp@gmail.com>
2017-09-06 09:57:14 -0700

    Question 2 complete

--- Quiz_0901/Program.cs
+++ Quiz_0901/Program.cs
@@ -10,6 +10,30 @@ namespace Quiz_0901
     {
         static void Main(string[] args)
         {
+            int count = 0;
+
+            Console.Write("Please enter a number: ");
+            string num1 = Console.ReadLine();
+            int userRes1 = int.Parse(num1);
+
+            Console.Write("Enter a second number: ");
+            string num2 = Console.ReadLine();
+            int userRes2 = int.Parse(num2);
+
+            if (userRes1 % 6 == 0)
+            {
+                Console.Write(userRes1 + ": ");
+                count++;
+            }
+
+            if (userRes2 % 6 == 0)
+            {
+                Console.Write(userRes2 + ": ");
+                count++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0} numbers were divisible by 6.", count);
+
         }
     }
 }
